023062432867c5296a35b802166d0776d0c83817
Yoeri-Vandevoorde <yoeri.vandevoorde@student.ucll.be>
2024-11-03 11:11:26 +0100

    added user model tests

--- back-end/test/model/user.test.ts
+++ back-end/test/model/user.test.ts
@@ -16,3 +16,47 @@ test('given: valid values for user, when: user is created, then: user is created
     expect(user.getUsername()).toBe(username);
     expect(user.getPassword()).toBe(password);
 });
+
+test('given: invalid values for user, when: user is created, then: error is thrown', () => {
+    // given
+    const username = '';
+    const password = 'john123';
+
+    // when then
+    expect(() => {
+        new User({
+            username,
+            password,
+        });
+    }).toThrow('Username is required'); 
+});
+
+test('given: too short of a password for user, when: user is created, then: error is thrown', () => {
+    // given
+    const username = 'john_doe';
+    const password = 'short'; // Invalid password (less than 6 characters)
+
+    // when then
+    expect(() => {
+        new User({
+            username,
+            password,
+        });
+    }).toThrow('Password must be at least 6 characters long'); 
+});
+
+test('given: invalid password for user, when: user is created, then: error is thrown', () => {
+    // given
+    const username = 'john_doe';
+    const password = ''; // Invalid password (less than 6 characters)
+
+    // when then
+    expect(() => {
+        new User({
+            username,
+            password,
+        });
+    }).toThrow('Password is required'); 
+});
+
+

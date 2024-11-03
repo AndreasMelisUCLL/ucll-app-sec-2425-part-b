16ead725c571e1cebf752a0b846e93b394f61978
Yoeri-Vandevoorde <yoeri.vandevoorde@student.ucll.be>
2024-11-03 16:28:16 +0100

    Create file structure, Create Header component

---
+++ front-end/components/header.tsx
@@ -0,0 +1,12 @@
+import styles from "@/styles/Home.module.css";
+const Header: React.FC = () => {
+    return (
+      <header className={`${styles.center}`}>
+        <a>
+          Chesser
+        </a>
+      </header>
+    );
+  };
+  
+  export default Header;

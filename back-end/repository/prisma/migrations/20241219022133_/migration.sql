0d0eca41e73005194eecc5b5b3f01346bdc8e52f
Yoeri-Vandevoorde <151052972+Yoeri-Vandevoorde@users.noreply.github.com>
2024-12-19 03:23:53 +0100

    initialise prisma

---
+++ back-end/repository/prisma/migrations/20241219022133_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `activeUserId` on the `Preset` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Preset" DROP COLUMN "activeUserId";

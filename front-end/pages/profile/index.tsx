12cdfc7749642b34cb21b3dc1f991c16b0dc24f1
Andreas Melis <andreas.melis@student.ucll.be>
2024-12-20 01:43:04 +0100

    improve readability and structure for Profile

--- front-end/pages/profile/index.tsx
+++ front-end/pages/profile/index.tsx
@@ -4,36 +4,40 @@ import Header from "@/components/Header";
 import ChessBoard from "@/components/ChessBoard";
 
 const Profile: React.FC = () => {
-  return (
-    <div className="min-h-full">
-      <Head>
-        <title>Boardflex | Profile</title>
-      </Head>
-      <Header activeTab="profile"/>
-      <main className={styles.main}>
-        <h1>Profile</h1>
-        <div className={styles["profile-container"]}>
-            <ChessBoard 
-            boardSize={600}
-            reskins={[
-                { piece: "wK", theme: { name: "california" } },
-                { piece: "wQ", theme: { name: "horsey" } },
-                { piece: "wR", theme: { name: "california" } },
-                { piece: "wB", theme: { name: "horsey" } },
-                { piece: "bP", theme: { name: "california" } },
-            ]}
-            perspective="black"
-            />
-            <div className={styles["user-info"]}>
-                <h2>User Information</h2>
-                <p>Name: John Doe</p>
-                <p>Email: john.doe@example.com</p>
-                <p>Bio: A passionate chess player and software developer. 
-                    Loves to solve complex problems and contribute to open-source projects.</p>
-            </div>
+    const reskins = [
+        { piece: "wK", theme: { name: "california" } },
+        { piece: "wQ", theme: { name: "horsey" } },
+        { piece: "wR", theme: { name: "california" } },
+        { piece: "wB", theme: { name: "horsey" } },
+        { piece: "bP", theme: { name: "california" } },
+    ];
+    const perspective = "black";
+
+    return (
+        <div className="min-h-full">
+            <Head>
+                <title>Boardflex | Profile</title>
+            </Head>
+
+            <Header activeTab="profile"/>
+            
+            <main className={styles.main}>
+                <h1>Profile</h1>
+                <div className={styles["profile-container"]}>
+                    <ChessBoard 
+                        reskins={reskins}
+                        perspective={perspective}
+                    />
+                    <div className={styles["user-info"]}>
+                        <h2>User Information</h2>
+                        <p>Name: John Doe</p>
+                        <p>Email: john.doe@example.com</p>
+                        <p>Bio: A passionate chess player and software developer. 
+                            Loves to solve complex problems and contribute to open-source projects.</p>
+                    </div>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
   );
 }
 
